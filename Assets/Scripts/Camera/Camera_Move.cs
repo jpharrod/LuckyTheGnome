24247de461a38bf5efcb2350450ec16ca5003435
James Harrod <jpharrod@gmail.com>
2016-11-12 01:45:22 -0800

    Big Update
    Added a basic level got some temp turrets in place with range markers.
    Added camera movement scripts as well.

---
+++ Assets/Scripts/Camera/Camera_Move.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Move : MonoBehaviour {
+
+    public int Movement_Modifier = 1;
+    public Vector3 Camera_NewTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Time.deltaTime * Movement_Modifier, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Time.deltaTime * Movement_Modifier * -1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, Time.deltaTime * Movement_Modifier);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, Time.deltaTime * Movement_Modifier * -1);
+        }
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
+        {
+            transform.Translate(0, Time.deltaTime * Movement_Modifier, 0);
+        }
+        else if (d < 0f && transform.position.y > 6.5)
+        {
+            transform.Translate(0, Time.deltaTime * Movement_Modifier * -1, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Main_Menu/Editor_Picker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Editor_Picker : MonoBehaviour {
+
+    void ClientButtonPressed()
+    {
+
+    }
+    void EditorButtonPressed()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
